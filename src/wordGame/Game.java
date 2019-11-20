26ba99f8e234496d3433efbdabdb6aa798f0ed3e
hughwilliams1 <hughwilliams2304@gmail.com>
2019-11-20 23:15:44 +0000

    Added initial classes

---
+++ src/wordGame/Game.java
@@ -0,0 +1,35 @@
+package wordGame;
+
+public class Game implements Controller {
+
+	@Override
+	public String refillRack() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String gameState() {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String play(Play play) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String calculateScore(Play play) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+	@Override
+	public String checkValidity(Play play) {
+		// TODO Auto-generated method stub
+		return null;
+	}
+
+}
---
+++ src/wordGame/Rack.java
@@ -0,0 +1,5 @@
+package wordGame;
+
+public class Rack {
+
+}
