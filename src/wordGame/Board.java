c8e1c1e50fd838e1a36564cd3083f024baec04c4
MaxAskew <max.askew@live.co.uk>
2019-11-23 11:22:09 +0000

    Implemented Board and Cell.

--- src/wordGame/Board.java
+++ src/wordGame/Board.java
@@ -1,5 +1,40 @@
 package wordGame;
 
 public class Board {
+	
+	private int width = 10, height = 10;
+	private Cell[][] board = new Cell [width][height];
+	
+	
+	public Board()
+	{
+		//Fill empty 2D array with Cells.
+		for (int x = 0; x<board.length;x++){
+		    for (int y = 0;y <board.length;y++)
+		    {
+		        board[x][y] = new Cell(x,y,false);
+		    }
+		}
+		
+		//Manually assign special cells.
+		board[4][1].setSpecial(true); //E2
+		board[5][1].setSpecial(true); //F2
+		
+		board[3][3].setSpecial(true); //D4
+		board[6][3].setSpecial(true); //G4
+		
+		board[3][6].setSpecial(true); //D7
+		board[6][6].setSpecial(true); //G7
+		
+		board[4][8].setSpecial(true); //E2
+		board[5][8].setSpecial(true); //F2
+	}
 
+	public Cell getCell(int x, int y) {
+		return board[x][y];
+	}
+	
+	public Cell[][] getBoard() {
+		return board;
+	}
 }
--- src/wordGame/Cell.java
+++ src/wordGame/Cell.java
@@ -1,5 +1,29 @@
 package wordGame;
 
 public class Cell {
+	
+	boolean isSpecial = false;
+	//Since the grid uses letters for the x this may need to change from an int.
+	//A = 0, B = 1 etc.
+	private int x,y;
+	private char value;
+	
+	public Cell (int x_,int y_,boolean isSpecial_) {
+		x = x_;
+		y = y_;
+		isSpecial = isSpecial_;
+	}
+	
+	public void setValue(char c) {
+		value = c;
+	}
 
+	public char getValue() {
+		return value;
+	}
+
+	public void setSpecial(boolean b) {
+		isSpecial = b;
+		
+	}
 }
